1b046738c3a859b761e1ca611e40f40a5eb070ee
Boris Denisenko <Shumer512@gmail.com>
2018-01-05 14:11:49 +0300

    Add base camera Activity. Update Main activity UI

---
+++ app/src/main/java/com/example/borisdenisenko/kotlin_opencv_counters/CameraActivity.kt
@@ -0,0 +1,25 @@
+package com.example.borisdenisenko.kotlin_opencv_counters
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Activity to preview camera stream with processed frames
+ */
+class CameraActivity : AppCompatActivity() {
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+
+}
--- app/src/main/java/com/example/borisdenisenko/kotlin_opencv_counters/MainActivity.kt
+++ app/src/main/java/com/example/borisdenisenko/kotlin_opencv_counters/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.borisdenisenko.kotlin_opencv_counters
 
+import android.Manifest
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -11,6 +12,13 @@ import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+
+        val CAPTURE_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        const val CAPTURE_PERMISSIONS_REQUEST = 101
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,8 +29,6 @@ class MainActivity : AppCompatActivity() {
                     .setAction("Action", null).show()
         }
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -41,17 +47,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
-
-    companion object {
-
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
-        }
-    }
 }
